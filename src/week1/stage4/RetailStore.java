8ce884af5763468abbb61fd9b9e2efb9343112b0
zachary <102142742+zlamb1@users.noreply.github.com>
2024-09-06 18:53:14 -0500

    Finished stage4

---
+++ src/week1/stage4/RetailStore.java
@@ -0,0 +1,41 @@
+package week1.stage4;
+
+import java.util.Arrays;
+
+public class RetailStore {
+    private CommercialItem[] items;
+
+    public RetailStore() {
+        items = new CommercialItem[3];
+    }
+
+    public CommercialItem[] getItems() {
+        return items;
+    }
+
+    public double orderItem(String itemName, int numUnitsToOrder) {
+        for (CommercialItem item : items) {
+            if (item != null && item.getItemDesc() != null && item.getItemDesc().equals(itemName)) {
+                return item.order(numUnitsToOrder);
+            }
+        }
+        return 0;
+    }
+
+    public double getTotalRevenue() {
+        double total = 0;
+        for (CommercialItem item : items) {
+            if (item != null) {
+                total += item.getRevenue();
+            }
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "RetailStore{" +
+                "items=" + Arrays.toString(items) +
+                '}';
+    }
+}
