88e4991d755423d1df0c5c34cac75a52f80f7c97
zachary <102142742+zlamb1@users.noreply.github.com>
2024-09-06 18:47:28 -0500

    Finished stage3

--- src/week1/stage2/Main.java
+++ src/week1/stage2/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         RetailStore retailStore = new RetailStore();
         retailStore.getItems()[0] = new CommercialItem("A basket of apples", 3, 10.0, 0);
-        retailStore.getItems()[1] = new CommercialItem("A jar of pickles", 13, 3.0, 0);;
-        retailStore.getItems()[2] = new CommercialItem("A bag of oranges", 7, 5.0, 0);;
+        retailStore.getItems()[1] = new CommercialItem("A jar of pickles", 13, 3.0, 0);
+        retailStore.getItems()[2] = new CommercialItem("A bag of oranges", 7, 5.0, 0);
         System.out.println(retailStore);
         for (CommercialItem item : retailStore.getItems()) {
             int unitsInStock = item.getUnitsInStock();
---
+++ src/week1/stage3/Main.java
@@ -0,0 +1,60 @@
+package week1.stage3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void displayInventory(RetailStore retailStore, boolean omitNoStockItems) {
+        if (retailStore != null) {
+            int counter = 1;
+            for (CommercialItem item : retailStore.getItems()) {
+                if (item != null && (!omitNoStockItems || item.getUnitsInStock() > 0)) {
+                    System.out.println(counter + ") " + item);
+                    counter++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RetailStore retailStore = new RetailStore();
+        retailStore.getItems()[0] = new CommercialItem("A basket of apples", 3, 10.0, 0);
+        retailStore.getItems()[1] = new CommercialItem("A jar of pickles", 13, 3.0, 0);
+        retailStore.getItems()[2] = new CommercialItem("A bag of oranges", 7, 5.0, 0);
+        while (true) {
+            System.out.println("Current stock:");
+            displayInventory(retailStore, true);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the name of the item to order (or 'q' to quit): ");
+            String itemDesc = scanner.nextLine();
+            if (itemDesc.equals("q")) {
+                break;
+            }
+            CommercialItem foundItem = null;
+            for (CommercialItem item : retailStore.getItems()) {
+                if (item != null && item.getItemDesc() != null && item.getItemDesc().equals(itemDesc)) {
+                    foundItem = item;
+                }
+            }
+            if (foundItem == null) {
+                System.out.println("Could not find an item with the name: '" + itemDesc + "'");
+                continue;
+            }
+            if (foundItem.getUnitsInStock() <= 0) {
+                System.out.println("That item is out of stock. Sorry!");
+                continue;
+            }
+            System.out.print("Enter the number of units to order: ");
+            int numberOfUnits = scanner.nextInt();
+            if (numberOfUnits < 1 || numberOfUnits > foundItem.getUnitsInStock()) {
+                System.out.println("Invalid number of units entered: " + numberOfUnits);
+                System.out.println("The number of units ordered must be between 1 - " + foundItem.getUnitsInStock());
+                continue;
+            }
+            double totalCharge = retailStore.orderItem(itemDesc, numberOfUnits);
+            System.out.println("Thank you for your purchase! The total charge is: " + totalCharge);
+        }
+        System.out.println("The total revenue is: " + retailStore.getTotalRevenue());
+        System.out.println("Current stock:");
+        displayInventory(retailStore, false);
+    }
+}
