8fb52605b4cc07477b6e3cd41aee10c3f01f2b52
zachary <102142742+zlamb1@users.noreply.github.com>
2024-09-13 00:11:45 -0500

    finished week 2 assignment

---
+++ src/week2/Main.java
@@ -0,0 +1,105 @@
+package week2;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+public class Main {
+    public static <T> T testFunction(Callable<T> callable) {
+        long start = System.nanoTime();
+        T result;
+        try {
+            result = callable.call();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            return null;
+        }
+        long duration = System.nanoTime() - start;
+        System.out.println("Execution time: " + (duration / 1000) + "μs");
+        return result;
+    }
+
+    public interface ISumUpToK {
+        String call(int[] arr, int k);
+    }
+
+    public interface IMoveZerosToFront {
+        int[] call(int[] arr);
+    }
+
+    public interface ISmallestDistanceNeighbors {
+        int call(int[] arr);
+    }
+
+    public interface IRemoveRepeats {
+        int[] call(int[] arr);
+    }
+
+    public static void testSumUpToK(String title, ISumUpToK sumUpToK) {
+        System.out.println(title + " -> ");
+        int[][] testInputs = {
+                { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+                { 30, 20, 10 },
+                { 1, 2, 0, 4, 5, 6, 7 }
+        };
+        int[] testInputs2 = { 5, 50, 6 };
+        for (int i = 0; i < testInputs.length; i++) {
+            System.out.println("Test " + (i + 1) + "). " + Arrays.toString(testInputs[i]));
+            int finalI = i;
+            System.out.println("Results: " + testFunction(() -> sumUpToK.call(testInputs[finalI], testInputs2[finalI])));
+        }
+        System.out.println();
+    }
+
+    public static void testMoveZerosToFront(String title, IMoveZerosToFront moveZerosToFront) {
+        System.out.println(title + " -> ");
+        int[][] testInputs = {
+                { 0, 0, 0, 1, 2, 3 },
+                { 1, 0, 2, 0, 3, 0 },
+                { 1, 0, 3, 0, 0, 7, 6 },
+                { 1, 2, 3, 0, 0, 0 }
+        };
+        for (int i = 0; i < testInputs.length; i++) {
+            System.out.println("Test " + (i + 1) + "). " + Arrays.toString(testInputs[i]));
+            int finalI = i;
+            System.out.println("Results: " + Arrays.toString(testFunction(() -> moveZerosToFront.call(testInputs[finalI]))));
+        }
+        System.out.println();
+    }
+
+    public static void testSmallestDistanceNeighbors(String title, ISmallestDistanceNeighbors smallestDistanceNeighbors) {
+        System.out.println(title + " -> ");
+        int[][] testInputs = {
+                { 2, 6, 8, 1, 3, 9, 7, 10 },
+        };
+        for (int i = 0; i < testInputs.length; i++) {
+            System.out.println("Test " + (i + 1) + "). " + Arrays.toString(testInputs[i]));
+            int finalI = i;
+            System.out.println("Results: " + testFunction(() -> smallestDistanceNeighbors.call(testInputs[finalI])));
+        }
+        System.out.println();
+    }
+
+    public static void testRemoveRepeats(String title, IRemoveRepeats removeRepeats) {
+        System.out.println(title + " -> ");
+        int[][] testInputs = {
+                { 3, 3, 3, 3 },
+                { 3, 2, 7, 2, 2, 6, 7 },
+        };
+        for (int i = 0; i < testInputs.length; i++) {
+            System.out.println("Test " + (i + 1) + "). " + Arrays.toString(testInputs[i]));
+            int finalI = i;
+            System.out.println("Results: " + Arrays.toString(testFunction(() -> removeRepeats.call(testInputs[finalI]))));
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        testSumUpToK("sumUpToK", Methods::sumUpToK);
+        testSumUpToK("sumUpToK2", Methods::sumUpToK2);
+        testSumUpToK("sumUpToK3", Methods::sumUpToK3);
+        testMoveZerosToFront("moveZerosToFront", Methods::moveZerosToFront);
+        testSmallestDistanceNeighbors("smallestDistanceNeighbors", Methods::smallestDistanceNeighbors);
+        testRemoveRepeats("removeRepeats", Methods::removeRepeats);
+        testRemoveRepeats("removeRepeats2", Methods::removeRepeats2);
+    }
+}
---
+++ src/week2/Methods.java
@@ -0,0 +1,123 @@
+package week2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Methods {
+    private static int getOccurrences(int[] arr, int k) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == k) {
+                count++;
+            }
+        }
+        return count;
+    }
+    // O(n^2)
+    // f(n) = n^2
+    public static String sumUpToK(int[] arr, int k) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] + arr[j] == k) {
+                    return arr[i] + " " + arr[j];
+                }
+            }
+        }
+        return "";
+    }
+    // O(n*k)
+    // f(n) = ceil(k / 2)(n + 1) + 1
+    // caveat: this method cannot handle negative values of k
+    public static String sumUpToK2(int[] arr, int k) {
+        HashMap<Integer, Integer> numOccurrences = new HashMap<>();
+        for (int i = 0; i <= k / 2; i++) {
+            int diff = k - i;
+            if (!numOccurrences.containsKey(i))
+                numOccurrences.put(i, getOccurrences(arr, i));
+            if (!numOccurrences.containsKey(diff))
+                numOccurrences.put(diff, getOccurrences(arr, diff));
+            if (i == diff) {
+                if (numOccurrences.get(i) > 1) {
+                    return i + " " + i;
+                }
+            } else if (numOccurrences.get(i) > 0 && numOccurrences.get(diff) > 0) {
+                return i + " " + diff;
+            }
+        }
+        return "";
+    }
+    // O(n)
+    // f(n) = 3n + 1
+    public static String sumUpToK3(int[] arr, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            int diff = k - i;
+            if (set.contains(diff)) {
+                return i + " " + diff;
+            }
+            set.add(i);
+        }
+        return "";
+    }
+    // O(n)
+    // f(n) = 2n + 1
+    public static int[] moveZerosToFront(int[] arr) {
+        int nonZeroCounter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                nonZeroCounter++;
+            } else if (nonZeroCounter > 0) {
+                arr[i] = arr[i - nonZeroCounter];
+                arr[i - nonZeroCounter] = 0;
+            }
+        }
+        return arr;
+    }
+    // O(n)
+    // f(n) = 8n + 2
+    static int smallestDistanceNeighbors(int[] arr) {
+        int minDistance = Integer.MAX_VALUE, index = -1;
+        for (int i = 1; i < arr.length - 1; i++) {
+            int dist = Math.abs(arr[i] - arr[i - 1]);
+            if (dist < minDistance) {
+                index = i - 1;
+                minDistance = dist;
+            }
+            dist = Math.abs(arr[i] - arr[i + 1]);
+            if (dist < minDistance) {
+                index = i;
+                minDistance = dist;
+            }
+        }
+        return index;
+    }
+    // O(n^2)
+    // f(n) = 3n + 2
+    static int[] removeRepeats(int[] arr) {
+        ArrayList<Integer> set = new ArrayList<>();
+        for (int i : arr) {
+            if (!set.contains(i)) {
+                set.add(i);
+            }
+        }
+        int[] result = new int[set.size()];
+        for (int i = 0; i < set.size(); i++) {
+            result[i] = set.get(i);
+        }
+        return result;
+    }
+    // O(n)
+    // f(n) = n + 1
+    static int[] removeRepeats2(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+        int[] newArray = new int[set.size()];
+        for (int i = 0; i < set.size(); i++) {
+            newArray[i] = set.iterator().next();
+        }
+        return newArray;
+    }
+}
